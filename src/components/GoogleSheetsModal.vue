bf32e93b9ea9b60647a08481c35847094f4cb68d
Banpot Sangsue <banpot@live.com>
2026-08-16 00:31:35 +0700

    added debug

--- src/components/GoogleSheetsModal.vue
+++ src/components/GoogleSheetsModal.vue
@@ -42,6 +42,8 @@ const handleFetchCSV = async (isSilent = false) => {
   try {
     const proxyUrl = '/api/sheets-proxy';
     const res = await fetch(proxyUrl);
+    console.log("res:", res);
+
     if (!res.ok) {
       throw new Error(`ไม่สามารถดึงข้อมูลได้ (Status: ${res.status})`);
     }
